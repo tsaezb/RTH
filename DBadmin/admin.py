b0ff88a99ca784f0bdbe3ccd132531b1d21eccf3
tsaezb <tomas.saezbin@gmail.com>
2015-10-25 20:19:21 -0300

    Modelo Paciente Terminado (v1.0)
    Ajustes a modelo Paciente
    +Se modifica campo "Sexo"
    Se añade modelo Comuna y Hospital.

--- DBadmin/admin.py
+++ DBadmin/admin.py
@@ -5,6 +5,8 @@ from .models import *
 admin.site.register(Paciente)
 admin.site.register(Enfermedad)
 admin.site.register(Habito)
+admin.site.register(Comuna)
+admin.site.register(Hospital)
 admin.site.register(Pretransplante)
 admin.site.register(AntecedentePretransplante)
 
