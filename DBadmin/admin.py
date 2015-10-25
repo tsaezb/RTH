5aa8c9bd612a3e2441f2ba836743f990a5a0ae36
dintrans <dintrans.g@gmail.com>
2015-10-25 19:31:15 -0300

    Modelo Eventos y Pretransplante

--- DBadmin/admin.py
+++ DBadmin/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import *
 
+class AntecedentePretransplanteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['nombre']}),
+        ]
+    search_fields = ['nombre']
+    raw_id_fields = ('nombre')
+    autocomplete_lookup_fields = {'m2m': ['nombre'],}
+
 admin.site.register(Paciente)
+admin.site.register(AntecedentePretransplante,AntecedentePretransplanteAdmin)
 # Register your models here.
--- DBadmin/models.py
+++ DBadmin/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Paciente(models.Model):
     nombre= models.CharField(max_length=200)
@@ -8,3 +9,32 @@ class Paciente(models.Model):
 
     
 # Create your models here.
+
+class Evento(models.Model):
+    tipo_de_evento_opciones= (('PreTX','PreTransplante'),
+                               ('TX','Transplante'),
+                               ('PostTX','PostTransplante'),
+                              )
+    tipo= models.CharField(max_length=100,choices=tipo_de_evento_opciones)
+    fecha= models.DateField('Fecha del Evento',default=timezone.now)
+
+class Transplante(Evento):
+    situacion_opciones= (('Electivo','Electivo'),
+                               ('Urgencia1','Urgencia Tipo1'),
+                               ('Urgencia2','Urgencia Tipo2'),
+                               ('Urgencia3','Urgencia Tipo3'),
+                              )
+    diagnostico= models.CharField(max_length=100)
+    causa_enlistamiento= models.CharField(max_length=100)
+    situacion= models.CharField(max_length=30,choices=situacion_opciones)
+    antecedentes_previos=models.ManyToManyField(AntecedentePretransplante)
+
+class AntecedentePretransplante(models.Model):
+    nombre= models.CharField(max_length=50)
+    class Meta:
+        verbose_name_plural = "Antecedentes Pretransplante"
+    def __str__(self):
+        return self.nombre
+    @staticmethod
+    def autocomplete_search_fields():
+        return ("nombre__iexact","nombre__icontains",)
