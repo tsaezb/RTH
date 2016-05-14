ed057c9e1d652c83465ef5d7e77361f93b155de8
tsaezb <tomas.saezbin@gmail.com>
2016-05-14 17:05:29 -0400

    adding library dependencies to patient form
    idem

--- static/js/config.js
+++ static/js/config.js
@@ -162,12 +162,35 @@ materialAdmin
                 resolve: {
                     loadPlugin: function($ocLazyLoad) {
                         return $ocLazyLoad.load ([
-                            {
-                                name: 'vendors',
-                                files: [
-                                    'vendors/bower_components/autosize/dist/autosize.min.js'
-                                ]
-                            }
+                          {
+                              name: 'css',
+                              insertBefore: '#app-level',
+                              files: [
+                                  'vendors/bower_components/nouislider/jquery.nouislider.css',
+                                  'vendors/farbtastic/farbtastic.css',
+                                  'vendors/bower_components/summernote/dist/summernote.css',
+                                  'vendors/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
+                                  'vendors/bower_components/chosen/chosen.min.css',
+                                  'vendors/bower_components/fullcalendar/dist/fullcalendar.min.css',
+
+                              ]
+                          },
+                          {
+                              name: 'vendors',
+                              files: [
+                                  'vendors/input-mask/input-mask.min.js',
+                                  'vendors/bower_components/nouislider/jquery.nouislider.min.js',
+                                  'vendors/bower_components/moment/min/moment.min.js',
+                                  'vendors/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+                                  'vendors/bower_components/summernote/dist/summernote.min.js',
+                                  'vendors/fileinput/fileinput.min.js',
+                                  'vendors/bower_components/chosen/chosen.jquery.js',
+                                  'vendors/bower_components/angular-chosen-localytics/chosen.js',
+                                  'vendors/bower_components/angular-farbtastic/angular-farbtastic.js',
+                                  'vendors/bower_components/autosize/dist/autosize.min.js'
+
+                              ]
+                          }
                         ])
                     }
                 }
